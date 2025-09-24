65668eb646e3108e256895dd0c0249dfa8aa5dbe
Mobn23 <mobn23@student.bth.se>
2025-09-24 09:45:03 +0200

    React router has been added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Message from "./Message";
+// import Message from "./Message";
 // import DocsList from "./components/DocsList";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RefactoredDocsList from "./components/RefactoredDocsList";
+import DocInfo from "./components/DocInfo";
 
 function App() {
   return (
-    <div>
-      <RefactoredDocsList></RefactoredDocsList>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<RefactoredDocsList/>} />
+          <Route path="/:id" element={<DocInfo/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
