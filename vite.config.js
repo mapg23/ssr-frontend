2f1a2329bb034fa4c2006d59d08bd5305a59ecfe
mapg23 <mapg23@student.bth.se>
2025-09-24 23:40:41 +0200

    added full url to base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ssr-frontend/',
+  base: '"https://mapg23.github.io/ssr-frontend/',
   plugins: [react()],
 })
